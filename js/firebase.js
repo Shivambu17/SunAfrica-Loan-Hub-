5ae8dfe1d96a0d7b7040c388ba1c66901b46a7fd
SHIVAMBU SHAUN <Shivambushaun0@gmail.com>
2026-05-10 01:20:04 +0200

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,18 +1,20 @@
-// js/firebase.js (updated with storage)
+// js/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBF1wM4iG8gdKjpTxYKFuKOjK4I2lbMUzQ",
+  authDomain: "sun-africa-loans.firebaseapp.com",
+  projectId: "sun-africa-loans",
+  storageBucket: "sun-africa-loans.firebasestorage.app",
+  messagingSenderId: "21690997002",
+  appId: "1:21690997002:web:f6f2c7ac8e0272d8b042f3",
+  measurementId: "G-ZTP1JXG67B"  // (optional, not used but kept)
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
